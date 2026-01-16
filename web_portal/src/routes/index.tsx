922828ef8b10ddf29199c6eba7059994b5de8786
obiabo <54102389+yhoungdev@users.noreply.github.com>
2026-01-17 00:35:33 +0100

    chore: added web_portal

---
+++ web_portal/src/routes/index.tsx
@@ -0,0 +1,14 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+
+export const Route = createFileRoute('/')({
+  component: App,
+})
+
+function App() {
+  return (
+    <div>
+     
+    </div>
+  )
+}
